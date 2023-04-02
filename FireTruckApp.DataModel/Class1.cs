05e00479452844e21f601844137c6d3ae5faff78
JP <Konry@users.noreply.github.com>
2023-04-02 19:56:04 +0000

    Add initial version

---
+++ FireTruckApp.DataModel/Class1.cs
@@ -0,0 +1,18 @@
+﻿
+namespace FireTruckApp.DataModel;
+
+public class FireTruck
+{
+    public List<Items> ItemsOnBoard{get; set;}
+    
+}
+
+public class Item{
+    public string Identifier{get;set;}
+
+    public double Weight{get;set;}
+
+    public List<string> Tags{get;set;}
+
+    public List<string> AlternateWording{get;set;}
+}
