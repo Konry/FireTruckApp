159ca170c42d612cdc31a4132531f1270c2b54a4
Jan Philipp Lührig <jpluehrig@web.de>
2023-05-10 01:02:43 +0200

    fix issues

--- BaseLibrary/InternalResourceLoader.cs
+++ BaseLibrary/InternalResourceLoader.cs
@@ -7,13 +7,27 @@ namespace BaseLibrary;
 
 public static class InternalResourceLoader
 {
+    /// <summary>
+    ///     Looks for an embedded resource in the calling assembly and returns the content as a text => usable for json test
+    ///     files
+    ///     You can add the folder by adding a "." between folder and filename
+    ///     filename.txt => looks in the assembly for filename.txt, returns the text of one
+    ///     folder.filename.txt => looks for files in the assembly in the corresponding folder, they can also be in an other
+    ///     folder
+    ///     Warning if there are several resources with the same name this will fail
+    /// </summary>
+    /// <param name="fileName">the file name to look for, can be with the folder</param>
+    /// <returns></returns>
+    /// <exception cref="EmbeddedResourceLoaderException">
+    ///     When file not exists or more than one file has the same name, or any
+    ///     other exception which occurred in this
+    /// </exception>
     public static string GetTextFromEmbeddedResource(string fileName)
     {
         try
         {
             Assembly assembly = Assembly.GetCallingAssembly();
-
-            string resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith($".{fileName}"));
+            string resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("." + fileName));
 
             using Stream? stream = assembly.GetManifestResourceStream(resourceName);
             if (stream == null)
